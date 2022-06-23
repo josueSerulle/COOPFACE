878a12c479034cd3365e3948cc1a0c6a7b8900f7
Josue Serulle Cabreja <jota_serulle@hotmail.com>
2022-06-22 20:32:32 -0400

    montar plantilla para la pagina principal

--- COOPFACE/settings.py
+++ COOPFACE/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landingPage'
 ]
 
 MIDDLEWARE = [
@@ -51,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'COOPFACE.urls'
 
+MYPAGE = os.path.join(BASE_DIR,"landingPage/public/views")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [MYPAGE],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
